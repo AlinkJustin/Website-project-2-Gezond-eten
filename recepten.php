ab6dbd68f9fc3c9467b7b683d3e76739e68110bb
AlinkJustin <71125271+AlinkJustin@users.noreply.github.com>
2020-11-26 20:30:29 +0100

    Added: plaatjes en text.

--- recepten.php
+++ recepten.php
@@ -44,10 +44,29 @@
                         </p>
                     </div>
                 </div>
-
                 <hr>
-
-
+                <div class="row">
+                    <div id="recept1" class="col col-33 col-m-100">
+                        <a><img src="images/recepten/lekker broodje ofzo.jpg"></a>
+                    </div>
+                    <div id="recept2" class="col col-33 col-m-100">
+                        <a><img src="images/recepten/pasta carbonara.jpg"></a>
+                    </div>
+                    <div id="recept3" class="col col-33 col-m-100">
+                    <a><img src="images/recepten/pasta met vis.jpg"></a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div id="recept4" class="col col-33 col-m-100">
+                        <a><img src="images/recepten/spruiten met ham.jpg"></a>
+                    </div>
+                    <div id="recept5" class="col col-33 col-m-100">
+                        <a><img src="images/recepten/taart.jpg"></a>
+                    </div>
+                    <div id="recept6" class="col col-33 col-m-100">
+                        <a><img src="images/recepten/img_000847_445x297_JPG.jpg"></a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
