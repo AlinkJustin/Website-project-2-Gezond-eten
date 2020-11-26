6a98b5fc020f76253401522b8df4cdda5a07c513
AlinkJustin <71125271+AlinkJustin@users.noreply.github.com>
2020-11-26 20:31:16 +0100

    Added: game additions.

--- scripts/FruitClicker.js
+++ scripts/FruitClicker.js
@@ -13,6 +13,31 @@ var items = [
         aantal: 0,
         basisPrijs: 500,
         aantalPerSeconde: 5,
+    },
+    {
+        aantal: 0,
+        basisPrijs: 2000,
+        aantalPerSeconde: 7.5,
+    },
+    {
+        aantal: 0,
+        basisPrijs: 5000,
+        aantalPerSeconde: 10,
+    },
+    {
+        aantal: 0,
+        basisPrijs: 10000,
+        aantalPerSeconde: 15,
+    },
+    {
+        aantal: 0,
+        basisPrijs: 20000,
+        aantalPerSeconde: 25,
+    },
+    {
+        aantal: 0,
+        basisPrijs: 50000,
+        aantalPerSeconde: 50,
     }
 ];
 var aantal = 0;
@@ -21,6 +46,11 @@ document.getElementById("hoofdfruit").addEventListener("click", clickedHoofdFrui
 document.getElementById("item1").addEventListener("click", clickedItem1);
 document.getElementById("item2").addEventListener("click", clickedItem2);
 document.getElementById("item3").addEventListener("click", clickedItem3);
+document.getElementById("item4").addEventListener("click", clickedItem4);
+document.getElementById("item5").addEventListener("click", clickedItem5);
+document.getElementById("item6").addEventListener("click", clickedItem6);
+document.getElementById("item7").addEventListener("click", clickedItem7);
+document.getElementById("item8").addEventListener("click", clickedItem8);
 
 function clickedHoofdFruit() {
     aantal += getAantalPerClick();
@@ -47,6 +77,26 @@ function clickedItem3() {
     var itemData = items[2];
     clickedItem(itemData);
 }
+function clickedItem4() {
+    var itemData = items[3];
+    clickedItem(itemData);
+}
+function clickedItem5() {
+    var itemData = items[4];
+    clickedItem(itemData);
+}
+function clickedItem6() {
+    var itemData = items[5];
+    clickedItem(itemData);
+}
+function clickedItem7() {
+    var itemData = items[6];
+    clickedItem(itemData);
+}
+function clickedItem8() {
+    var itemData = items[7];
+    clickedItem(itemData);
+}
 function updateStats() {
     document.getElementById("fruitAantal").innerText = Math.floor(aantal);
     document.getElementById("item1Prijs").innerText = items[0].basisPrijs;
@@ -55,6 +105,16 @@ function updateStats() {
     document.getElementById("item2Aantal").innerText = items[1].aantal;
     document.getElementById("item3Prijs").innerText = items[2].basisPrijs;
     document.getElementById("item3Aantal").innerText = items[2].aantal;
+    document.getElementById("item4Prijs").innerText = items[3].basisPrijs;
+    document.getElementById("item4Aantal").innerText = items[3].aantal;
+    document.getElementById("item5Prijs").innerText = items[4].basisPrijs;
+    document.getElementById("item5Aantal").innerText = items[4].aantal;
+    document.getElementById("item6Prijs").innerText = items[5].basisPrijs;
+    document.getElementById("item6Aantal").innerText = items[5].aantal;
+    document.getElementById("item7Prijs").innerText = items[6].basisPrijs;
+    document.getElementById("item7Aantal").innerText = items[6].aantal;
+    document.getElementById("item8Prijs").innerText = items[7].basisPrijs;
+    document.getElementById("item8Aantal").innerText = items[7].aantal;
     document.getElementById("fruitPerSeconde").innerText = getAantalPerSeconde();
 }
 function getAantalPerClick() {
--- svvgame.php
+++ svvgame.php
@@ -45,24 +45,89 @@
                 </div>
 
                 <hr>
-                <div>
-                    <input type="button" id="hoofdfruit" value="Fruit">
-                    <input type="button" id="item1" value="Item 1">
-                    <input type="button" id="item2" value="Item 2">
-                    <input type="button" id="item3" value="Item 3">
-                    <div class="stats">
-
-                        aantal Fruit: <span id="fruitAantal"></span><br>
-                        prijs Item 1: <span id="item1Prijs"></span><br>
-                        aantal Item 1: <span id="item1Aantal"></span><br>
-                        prijs Item 2: <span id="item2Prijs"></span><br>
-                        aantal Item 2: <span id="item2Aantal"></span><br>
-                        prijs Item 3: <span id="item3Prijs"></span><br>
-                        aantal Item 3: <span id="item3Aantal"></span><br>
-                        Fps: <span id="fruitPerSeconde"></span><br>
+                <div class="row" id="game-container">
+                    <div class="col col-50 col-m-100">
+                        <div class="center-block">
+                            <div><h2>aantal Fruit: <h2><span id="fruitAantal"></span></div>
+                            <div id="hoofdfruit"></div>
+                            <div><h2>Fps: </h2><span id="fruitPerSeconde"></span></div>
+                        </div>
+                    </div>
+                    <div class="col col-50 col-m-100">
+                        <div class="row">
+                            <div class="col col-50 col-m-100">
+                                <input type="button" class="upgrade-button" id="item1" value="Item 1">
+                            </div>
+                            <div class="col col-50 col-m-100">
+                                <div>prijs Item 1: <span id="item1Prijs"></span></div>
+                                <div>aantal Item 1: <span id="item1Aantal"></span></div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col col-50 col-m-100">
+                                <input type="button" class="upgrade-button" id="item2" value="Item 2">
+                            </div>
+                            <div class="col col-50 col-m-100">
+                                <div>prijs Item 2: <span id="item2Prijs"></span></div>
+                                <div>aantal Item 2: <span id="item2Aantal"></span></div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col col-50 col-m-100">
+                                <input type="button" class="upgrade-button" id="item3" value="Item 3">
+                            </div>
+                            <div class="col col-50 col-m-100">
+                                <div>prijs Item 3: <span id="item3Prijs"></span></div>
+                                <div>aantal Item 3: <span id="item3Aantal"></span></div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col col-50 col-m-100">
+                                <input type="button" class="upgrade-button" id="item4" value="Item 4">
+                            </div>
+                            <div class="col col-50 col-m-100">
+                                <div>prijs Item 4: <span id="item4Prijs"></span></div>
+                                <div>aantal Item 4: <span id="item4Aantal"></span></div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col col-50 col-m-100">
+                                <input type="button" class="upgrade-button" id="item5" value="Item 5">
+                            </div>
+                            <div class="col col-50 col-m-100">
+                                <div>prijs Item 5: <span id="item5Prijs"></span></div>
+                                <div>aantal Item 5: <span id="item5Aantal"></span></div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col col-50 col-m-100">
+                                <input type="button" class="upgrade-button" id="item6" value="Item 6">
+                            </div>
+                            <div class="col col-50 col-m-100">
+                                <div>prijs Item 6: <span id="item6Prijs"></span></div>
+                                <div>aantal Item 6: <span id="item6Aantal"></span></div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col col-50 col-m-100">
+                                <input type="button" class="upgrade-button" id="item7" value="Item 7">
+                            </div>
+                            <div class="col col-50 col-m-100">
+                                <div>prijs Item 7: <span id="item7Prijs"></span></div>
+                                <div>aantal Item 7: <span id="item7Aantal"></span></div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col col-50 col-m-100">
+                                <input type="button" class="upgrade-button" id="item8" value="Item 8">
+                            </div>
+                            <div class="col col-50 col-m-100">
+                                <div>prijs Item 8: <span id="item8Prijs"></span></div>
+                                <div>aantal Item 8: <span id="item8Aantal"></span></div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
